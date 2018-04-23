491752db264ac2e2faf441a435ff0dbd62069efa
Samuel <tobisamuel88@gmail.com>
2018-04-23 02:46:26 +0100

    'deploy'

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 var path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 
 app.get('/*', (req,res)=>{
-  res.sendFile(path.join(__dirname, 'dist/index.html'))
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
 app.listen(process.env.PORT || 3000, (err)=>{
