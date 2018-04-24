4052dfb11b6ad9b2e35df6d4cd9d6652d4c6c50e
Samuel <tobisamuel88@gmail.com>
2018-04-24 21:05:09 +0100

    'deploy'

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/index', pathMatch: 'full' },
+  { path: 'index',  component: LandingPageComponent }
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +18,8 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
     LandingPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
